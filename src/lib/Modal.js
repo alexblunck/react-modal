5728d971bc53fd9cfeb842d400779127b129862c
Alexander Blunck <alex@blunck.co>
2017-11-22 15:49:43 +0100

    Modal:
    - Added custom scrollbar
    - Isolate scrolling when visible
    - Display a header border when scrolling
    - Allow modal to shrink if window height is smaller than modal height
    - Tweaked header title color

--- src/lib/Modal.js
+++ src/lib/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import PerfectScrollbar from 'perfect-scrollbar'
 import Modals from './Modals'
 import IconDismiss from './IconDismiss'
 
@@ -7,14 +8,47 @@ export default class Modal extends React.Component {
 
     constructor(props) {
         super(props)
+
         this.state = {}
+
+        this.onScrollDown = this.onScrollDown.bind(this)
+        this.onScrollToTop = this.onScrollToTop.bind(this)
+    }
+
+    componentDidMount() {
+        this.contentElem = this.elem.querySelector('.modal-content')
+
+        this.ps = new PerfectScrollbar(this.contentElem, {
+            suppressScrollX: true
+        })
+
+        this.contentElem.addEventListener('ps-scroll-down', this.onScrollDown)
+        this.contentElem.addEventListener('ps-y-reach-start', this.onScrollToTop)
+    }
+
+    componentDidUpdate(prevProps) {
+        // Update scrollbar / header border if component changes
+        if(prevProps.component !== this.props.component) {
+            this.ps.update()
+            this.onScrollToTop()
+        }
+    }
+
+    componentWillUnmount() {
+        this.ps.destroy()
+        this.ps = null
+        this.contentElem.removeEventListener('ps-scroll-down', this.onScrollDown)
+        this.contentElem.removeEventListener('ps-y-reach-start', this.onScrollToTop)
     }
 
     render() {
         return (
-            <div className="Modal">
+            <div className='Modal' ref={e => this.elem = e}>
                 {this.renderHeader()}
-                {this.renderContent()}
+
+                <div className="modal-content">
+                    {this.renderContent()}
+                </div>
             </div>
         )
     }
@@ -41,16 +75,33 @@ export default class Modal extends React.Component {
         if (this.props.component) {
             const Content = this.props.component
 
-            return (
-                <div className="modal-content">
-                    <Content />
-                </div>
-            )
+            return <Content />
         }
     }
 
+    /**
+     * dismiss modal.
+     */
     dismiss() {
         Modals.dismiss()
+    }
+
+    /**
+     * Handle ps-scroll-down event.
+     *
+     * @param {Event} event
+     */
+    onScrollDown (event) {
+        this.elem.classList.add('show-header-border')
+    }
+
+    /**
+     * Handle ps-y-reach-star event.
+     *
+     * @param {Event} event
+     */
+    onScrollToTop (event) {
+        this.elem.classList.remove('show-header-border')
     }
 
 }
